4d898772cb2b74fca5ee029284cf873f791ce8f9
PeterRapcsak <peter.rapcsak@gmail.com>
2023-12-15 09:24:01 +0100

    base
    base

--- mail.js
+++ mail.js
@@ -1,8 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
  
-fs.readFile('apikeys.txt', (err, data) => {
+fs.readFile('D:/code/apikeys.txt', (err, data) => {
     if (err) throw err;
  
     console.log(data.toString());
 })
 
+const nodeMailer = require('nodemailer');
+
+const html = `
+    <h1>Hello World</h1>
+    <p>Nodemailer message</p>
+`;
+
+async function main() {
+
+    nodeMailer.createTransport({
+        host: "mail.openjavascript.info",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "open.jscode@gmail.com",
+            pass: "a"
+        }
+
+    });
+
+}
+
+
+main();
